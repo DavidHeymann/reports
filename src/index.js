dc40ab454e4274141fdbfa164cf8c6455873ee1b
DavidHeymann <heymann.david@gmail.com>
2019-05-20 20:41:06 +0300

    SeasonDisplay

--- src/index.js
+++ src/index.js
@@ -1,31 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
-import faker from "faker";
+import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-  return (
-    <div class="ui comments">
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author="Sam"
-        content="First comment"
-        date={faker.date.recent().toLocaleString()}
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author="Alex"
-        content="Second comment"
-        date={faker.date.recent().toLocaleString()}
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author="Jane"
-        content="Third comment"
-        date={faker.date.recent().toLocaleString()}
-      />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { lat: null, error: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ error: err.message })
+    );
+  }
+  render() {
+    if (this.state.lat && !this.state.error) {
+      return <div><SeasonDisplay lat={this.state.lat}/></div>;
+    } else if (this.state.error) {
+      return <div>Error: {this.state.error}</div>;
+    }
+    return <div>Loading!</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
