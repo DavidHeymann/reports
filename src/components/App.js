7acec3aff68f546167f6a2184929378b68e87529
DavidHeymann <heymann.david@gmail.com>
2019-06-17 19:18:06 +0300

    simple yt

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import unsplash from "../api/unsplash";
-import SearchBar from "./SearchBar";
-import ImageList from './ImageList';
+import SearchBar from './SearchBarYT';
 
 class App extends React.Component {
-  state = { images: [] };
-
-  onSearchSubmmit = async term => {
-    const response = await unsplash.get("/search/photos", {
-      params: { query: term }
-    });
-
-    this.setState({images: response.data.results});
-  };
-
-  render() {
-    return (
-      <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmmit={this.onSearchSubmmit} />
-        <ImageList images={this.state.images} /> 
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="ui container">
+                <SearchBar />
+                </div>
+        );
+    }
 }
 
-export default App;
+export default  App;
